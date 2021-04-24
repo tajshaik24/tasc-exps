a96b592b081225716cedd65978f5a25f783b75d5
Taj Shaik <tshaik@berkeley.edu>
2021-04-24 13:02:15 -0700

    Fixing script

--- lambdas/tasc-lambda/upload-tasc-lambda.sh
+++ lambdas/tasc-lambda/upload-tasc-lambda.sh
@@ -1,16 +1,16 @@
 NAME=tasc-lambda
-CREATE_LAMBDA=false
+CREATE_LAMBDA=1
 while getopts cn: flag
 do
     case "${flag}" in
-        c) CREATE_LAMBDA=true;;
+        c) CREATE_LAMBDA=0;;
         n) NAME=${OPTARG};;
     esac
 done
 zip -r $NAME.zip .
 aws s3 cp $NAME.zip s3://tasc-lambdas
-if [[ CREATE_LAMBDA ]]
-    then aws lambda create-function --function-name $NAME --runtime python3.7 --role arn:aws:iam::861473675836:role/TASCLambda --handler lambda_function.lambda_handler --timeout 120 --memory-size 2048 --code "{'S3Bucket':'tasc-lambdas','S3Key':'${NAME}.zip'}"
+if [ $CREATE_LAMBDA == 0 ]
+    then aws lambda create-function --function-name $NAME --runtime python3.7 --role arn:aws:iam::861473675836:role/TASCLambda --handler lambda_function.lambda_handler --timeout 120 --memory-size 2048 --code {"S3Bucket":"tasc-lambdas","S3Key":"${NAME}.zip"}
 else
     aws lambda update-function-code --s3-bucket "tasc-lambdas" --s3-key "${NAME}.zip" --function-name $NAME
 fi 
