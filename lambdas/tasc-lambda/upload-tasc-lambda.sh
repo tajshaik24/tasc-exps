b6c6c94f675d50cd1469b1cceee11ff19e51c2e8
Taj Shaik <tshaik@berkeley.edu>
2021-04-29 20:07:58 -0700

    fixing script

--- lambdas/tasc-lambda/upload-tasc-lambda.sh
+++ lambdas/tasc-lambda/upload-tasc-lambda.sh
@@ -12,7 +12,7 @@ done
 zip -r $NAME.zip .
 aws s3 cp $NAME.zip s3://tasc-lambdas
 if [ $CREATE_LAMBDA == 0 ]
-    then aws lambda create-function --function-name $NAME --runtime python3.7 --role arn:aws:iam::861473675836:role/TASCLambda --handler ${HANDLER} --timeout 120 --memory-size 2048 --code {"S3Bucket":"tasc-lambdas","S3Key":"${NAME}.zip"}
+    then aws lambda create-function --function-name $NAME --runtime python3.7 --role arn:aws:iam::861473675836:role/TASCLambda --handler ${HANDLER} --timeout 120 --memory-size 2048 --code S3Bucket=tasc-lambdas,S3Key=${NAME}.zip
 else
     aws lambda update-function-code --s3-bucket "tasc-lambdas" --s3-key "${NAME}.zip" --function-name $NAME
 fi 
