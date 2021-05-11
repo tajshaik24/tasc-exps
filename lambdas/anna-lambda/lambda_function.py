e847e2e4639bf59e628ba6d1453e9cafb6de1d95
saurav_c <sauravc@berkeley.edu>
2021-05-10 19:44:54 -0700

    More indents

--- lambdas/anna-lambda/lambda_function.py
+++ lambdas/anna-lambda/lambda_function.py
@@ -50,83 +50,81 @@ def lambda_handler(event, context):
 
 		# Perform routing lookups
 		for _ in range(num_lookups):
-    		port = 6450
-    		start = time.time()
-    		addresses = dumb_client._query_routing(key, port)
-    		end = time.time()
+			port = 6450
+			start = time.time()
+			addresses = dumb_client._query_routing(key, port)
+			end = time.time()
 
-    		lookup_times.append(end - start)
-    		throughput_time += (end - start)
+			lookup_times.append(end - start)
+			throughput_time += (end - start)
 
-    	# Perform writes
-    	for _ in range(num_writes):
-    		key = prefix + str(bounded_zipf.rvs(size=1)[0])
+		# Perform writes
+		for _ in range(num_writes):
+			key = prefix + str(bounded_zipf.rvs(size=1)[0])
 
-    		port = random.choice([6450, 6451, 6452, 6453])
-    		addresses = dumb_client._query_routing(key, port)
-    		send_sock = dumb_client.pusher_cache.get(addresses[0])
+			port = random.choice([6450, 6451, 6452, 6453])
+			addresses = dumb_client._query_routing(key, port)
+			send_sock = dumb_client.pusher_cache.get(addresses[0])
 
-    		data = os.urandom(4096)
-    		lww = LWW(time.time_ns(), data)
-    		req, tup = dumb_client._prepare_data_request([key])
-    		req.type = PUT
+			data = os.urandom(4096)
+			lww = LWW(time.time_ns(), data)
+			req, tup = dumb_client._prepare_data_request([key])
+			req.type = PUT
 
-    		rids = [req.request_id]
-        	
-        	tup = tup[0]
-            tup.payload, tup.lattice_type = dumb_client._serialize(value)
+			rids = [req.request_id]
+			tup = tup[0]
+			tup.payload, tup.lattice_type = dumb_client._serialize(value)
 
-            start = time.time()
-            send_request(req, send_sock)
-            responses = recv_response(rids, dumb_client.response_puller, KeyResponse)
-            end = time.time()
+			start = time.time()
+			send_request(req, send_sock)
+			responses = recv_response(rids, dumb_client.response_puller, KeyResponse)
+			end = time.time()
 
-            write_times.append(end - start)
-            throughput_time += (end - start)
+			write_times.append(end - start)
 
-    	# Perform reads
-    	for _ in range(num_reads):
-    		key = prefix + str(bounded_zipf.rvs(size=1)[0])
+			throughput_time += (end - start)
 
-    		port = random.choice([6450, 6451, 6452, 6453])
-    		addresses = dumb_client._query_routing(key, port)
-    		send_sock = dumb_client.pusher_cache.get(addresses[0])
+		# Perform reads
+		for _ in range(num_reads):
+			key = prefix + str(bounded_zipf.rvs(size=1)[0])
 
-    		req, _ = dumb_client._prepare_data_request([key])
-    		req.type = GET
+			port = random.choice([6450, 6451, 6452, 6453])
+			addresses = dumb_client._query_routing(key, port)
+			send_sock = dumb_client.pusher_cache.get(addresses[0])
 
-    		rids = [req.request_id]
+			req, _ = dumb_client._prepare_data_request([key])
+			req.type = GET
+			rids = [req.request_id]
 
-    		start = time.time()
-    		send_request(req, send_sock)
+			start = time.time()
 
-        	# Wait for all responses to return.
-        	responses = recv_response(rids, dumb_client.response_puller, KeyResponse)
-    		end = time.time()
+			send_request(req, send_sock)
 
-        	read_times.append(end - start)
-        	throughput_time += (end - start)
+			# Wait for all responses to return.
 
+			responses = recv_response(rids, dumb_client.response_puller, KeyResponse)
+			end = time.time()
 
-    throughput = (num_txns * (num_writes + num_reads)) / throughput_time
-    
-    convert = lambda x: x * 1000
-    read_lat = list(map(convert, read_times))
-    write_lat = list(map(convert, write_times))
-    lookup_lat = list(map(convert, lookup_times))
+			read_times.append(end - start)
+			throughput_time += (end - start)
 
-    read_msg = ",".join(map(str, read_lat))
-    write_msg = ",".join(map(str, write_lat))
-    lookup_msg = ",".join(map(str, lookup_lat))
+	throughput = (num_txns * (num_writes + num_reads)) / throughput_time
+	convert = lambda x: x * 1000
+	read_lat = list(map(convert, read_times))
+	write_lat = list(map(convert, write_times))
+	lookup_lat = list(map(convert, lookup_times))
+	read_msg = ",".join(map(str, read_lat))
+	write_msg = ",".join(map(str, write_lat))
+	lookup_msg = ",".join(map(str, lookup_lat))
 
-    ctx = zmq.Context(1)
-    sckt = ctx.socket(zmq.PUSH)
-    sckt.connect('tcp://%s:6600' % benchmark_server)
+	ctx = zmq.Context(1)
+	sckt = ctx.socket(zmq.PUSH)
+	sckt.connect('tcp://%s:6600' % benchmark_server)
 
-    message = str(throughput) + ";" + str(read_msg) + ";" + str(write_msg) + ";" + str(lookup_msg)
-    sckt.send_string(message)
+	message = str(throughput) + ";" + str(read_msg) + ";" + str(write_msg) + ";" + str(lookup_msg)
 
-    return "Success"
+	sckt.send_string(message)
+	return "Success"
 
 
 
